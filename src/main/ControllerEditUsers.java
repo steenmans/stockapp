a0797cdd1496a8ea2c99a5130cf69c0556260c9a
samst <samsteenmans@hotmail.com>
2020-08-13 21:35:03 +0200

    tableview edit user inorde

--- src/main/ControllerEditUsers.java
+++ src/main/ControllerEditUsers.java
@@ -5,6 +5,8 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 
 import java.sql.*;
 
@@ -12,13 +14,13 @@ public class ControllerEditUsers {
     @FXML
     private TableView<User> editUserTableView;
     @FXML
-    private TableColumn<User,String> editUserTableColumnName;
+    private TableColumn<User, String> editUserTableColumnName;
     @FXML
-    private TableColumn<User,Boolean> editUserTableColumnAdmin;
+    private TableColumn<User, Boolean> editUserTableColumnAdmin;
     @FXML
     private TextField editUserNameTextField;
     @FXML
-    private PasswordField editUserPassword1PasswordField,editUserPassword2PasswordField;
+    private PasswordField editUserPassword1PasswordField, editUserPassword2PasswordField;
     @FXML
     private CheckBox editUserAdminCheckbox;
 
@@ -28,7 +30,7 @@ public class ControllerEditUsers {
     public static class MyConnection {
 
         // create a function to connect with mysql database
-        public static Connection getConnection(){
+        public static Connection getConnection() {
 
             Connection con = null;
             try {
@@ -44,37 +46,56 @@ public class ControllerEditUsers {
     }
 
     @FXML
-    public void initialize(){
+    public void initialize() {
         String mysqlSelect = "SELECT * FROM login";
         Statement st;
         ResultSet rs;
 
-
+        //data binnenhalen.
         try {
             st = MyConnection.getConnection().createStatement();
 
             rs = st.executeQuery(mysqlSelect);
 
-            while (rs.next()){
-                observableListArraylist.add(new User(rs.getString("username"),rs.getString("password"),rs.getBoolean("admin")));
+            while (rs.next()) {
+                observableListArraylist.add(new User(rs.getString("username"), rs.getString("password"), rs.getBoolean("admin")));
             }
 
 
-        }catch (SQLException e){
+        } catch (SQLException e) {
             e.getCause().printStackTrace();
         }
 
 
-
-
-
-
-
-        editUserTableColumnName.setCellValueFactory(new PropertyValueFactory<User,String>("name"));
-        editUserTableColumnAdmin.setCellValueFactory(new PropertyValueFactory<User,Boolean>("admin"));
+        editUserTableColumnName.setCellValueFactory(new PropertyValueFactory<User, String>("name"));
+        editUserTableColumnAdmin.setCellValueFactory(new PropertyValueFactory<User, Boolean>("admin"));
         editUserTableView.setItems(observableListArraylist);
+
+        //Mouse and key
+        editUserTableView.setOnMouseClicked((MouseEvent event) -> {
+            if (event.getClickCount() > 1) {
+                onEdit();
+            }
+        });
+        editUserTableView.setOnKeyReleased((KeyEvent event) -> {
+                onEdit();
+        });
+
     }
 
-
-
+    public void onEdit() {
+        // check the table's selected item and get selected item
+        if (editUserTableView.getSelectionModel().getSelectedItem() != null) {
+            User user = editUserTableView.getSelectionModel().getSelectedItem();
+            editUserNameTextField.setText(user.getName());
+            editUserPassword1PasswordField.setText(user.getPassword());
+            if(user.isAdmin()){
+                editUserAdminCheckbox.setSelected(true);
+            }
+        }
+    }
 }
+
+
+
+
